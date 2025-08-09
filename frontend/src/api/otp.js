21b6d078d9fb674ebe6030ffc00a5463189eaea7
Mahesh Jadhao <maheshjadhao172@gmail.com>
2025-08-09 10:27:07 +0530

    solve error

--- frontend/src/api/otp.js
+++
@@ -1,8 +0,0 @@
-import { registerUser, verifyOtp } from "./auth";
-
-// For registration, use registerUser(data)
-// For OTP verification, use verifyOtp(email, otp)
-
-// Example usage:
-// await registerUser({ ... });
-// await verifyOtp(email, otp);
